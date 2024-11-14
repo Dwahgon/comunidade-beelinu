008d317978af8b9d4692660821fe865c256a2a9d
Rian <riancarlosvalcanaia99@gmail.com>
2024-11-14 17:27:03 -0300

    feat:RoomList + newUserFix

--- levels/main_menu/join.gd
+++ levels/main_menu/join.gd
@@ -9,9 +9,15 @@ func _load_servers():
 	var servers = []
 	servers.append_array(rooms_res["servers"])
 	servers.append_array(rooms_res["temporaryServers"])
-	
+	for filho in %RoomList.get_children():
+		filho.queue_free()
+		
 	for server in servers:
 		var server_button = JOIN_BUTTON_SCENE.instantiate()
 		server_button.text = server["server_name"]
 		%RoomList.add_child(server_button)
 	
+
+func _on_reload_button_pressed() -> void:
+	print("Carregando servidores...")
+	_load_servers()
